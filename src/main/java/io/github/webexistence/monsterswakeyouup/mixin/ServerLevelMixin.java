d0a340395aa631162ed9a8c524365383c06bcec1
domi <scavcrab@gmail.com>
2026-09-07 09:49:24 -0500

    Rename static constants to follow CAPS naming convention

--- src/main/java/io/github/webexistence/monsterswakeyouup/mixin/ServerLevelMixin.java
+++ src/main/java/io/github/webexistence/monsterswakeyouup/mixin/ServerLevelMixin.java
@@ -27,11 +27,11 @@ import java.util.function.BooleanSupplier;
 @Mixin(ServerLevel.class)
 public abstract class ServerLevelMixin {
     @Unique
-    private static final int minSpawnDistance = 2;
+    private static final int MIN_SPAWN_DISTANCE = 2;
     @Unique
-    private static final int maxSpawnDistance = 32;
+    private static final int MAX_SPAWN_DISTANCE = 32;
     @Unique
-    private static final int maxSpawnDistanceVertical = 15;
+    private static final int MAX_SPAWN_DISTANCE_VERTICAL = 15;
 
     @Unique
     ServerLevel serverLevel = (ServerLevel) (Object) this;
@@ -97,17 +97,17 @@ public abstract class ServerLevelMixin {
                 do {
                     if (i < numSpawnAttempts - lastAttempts) {
                         // regular attempts
-                        spawnX = serverLevel.random.nextInt(maxSpawnDistance) - serverLevel.random.nextInt(maxSpawnDistance);
-                        spawnZ = serverLevel.random.nextInt(maxSpawnDistance) - serverLevel.random.nextInt(maxSpawnDistance);
+                        spawnX = serverLevel.random.nextInt(MAX_SPAWN_DISTANCE) - serverLevel.random.nextInt(MAX_SPAWN_DISTANCE);
+                        spawnZ = serverLevel.random.nextInt(MAX_SPAWN_DISTANCE) - serverLevel.random.nextInt(MAX_SPAWN_DISTANCE);
                     } else {
                         // lastAttempts use reduced range
-                        spawnX = serverLevel.random.nextInt(maxSpawnDistance / 2 - 1) - serverLevel.random.nextInt(maxSpawnDistance / 4 - 1);
-                        spawnZ = serverLevel.random.nextInt(maxSpawnDistance / 2 - 1) - serverLevel.random.nextInt(maxSpawnDistance / 4 - 1);
+                        spawnX = serverLevel.random.nextInt(MAX_SPAWN_DISTANCE / 2 - 1) - serverLevel.random.nextInt(MAX_SPAWN_DISTANCE / 4 - 1);
+                        spawnZ = serverLevel.random.nextInt(MAX_SPAWN_DISTANCE / 2 - 1) - serverLevel.random.nextInt(MAX_SPAWN_DISTANCE / 4 - 1);
                     }
                     squaredDistance = (spawnX * spawnX) + (spawnZ * spawnZ);
                     // squaredDistance is Pythagorean theorem: x^2 + z^2 = distance^2
-                } while (squaredDistance <= minSpawnDistance
-                        || squaredDistance >= (maxSpawnDistance * maxSpawnDistance));
+                } while (squaredDistance <= MIN_SPAWN_DISTANCE
+                        || squaredDistance >= (MAX_SPAWN_DISTANCE * MAX_SPAWN_DISTANCE));
 
                 int mobPosX = playerPosX + spawnX;
                 int mobPosZ = playerPosZ + spawnZ;
@@ -124,8 +124,8 @@ public abstract class ServerLevelMixin {
                 MobSpawnType mobSpawnType = MobSpawnType.NATURAL;
 
                 // scan for valid Y coordinate; change BlockPos if one is found
-                int maxSpawnY = playerPosY + maxSpawnDistanceVertical;
-                int minSpawnY = playerPosY - maxSpawnDistanceVertical;
+                int maxSpawnY = playerPosY + MAX_SPAWN_DISTANCE_VERTICAL;
+                int minSpawnY = playerPosY - MAX_SPAWN_DISTANCE_VERTICAL;
                 boolean foundValidPosY = false;
                 for (int y = maxSpawnY; y > minSpawnY; y--) {
                     mobSpawnBlockPos =  new BlockPos(mobPosX, y, mobPosZ);
@@ -155,7 +155,7 @@ public abstract class ServerLevelMixin {
                     // Create mob object to do pathfinding check
                     Mob mob = (Mob) entity;
                     mob.setOnGround(true); // necessary for createPath() to return non-null
-                    Path path = mob.getNavigation().createPath(player.blockPosition(), 1, maxSpawnDistance);
+                    Path path = mob.getNavigation().createPath(player.blockPosition(), 1, MAX_SPAWN_DISTANCE);
 
                     if (path == null || !path.canReach()) {
                         LOGGER.info("Mob ({}) could not pathfind to player {}. Cancelling spawn.", mob.getName().getString(), player.getName().getString());
